717a82276e60d12c4db932973ce111ff8018679e
eshan-sharma <43044334+Eshan-Sharma@users.noreply.github.com>
2024-04-13 01:08:18 +0530

    added basic route structure

--- backend/src/routes/blog.ts
+++ backend/src/routes/blog.ts
@@ -1,2 +1,27 @@
 import { Hono } from "hono";
-export const blogRouter = new Hono();
+import { sign } from "hono/jwt";
+import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
+export const blogRouter = new Hono<{
+  Bindings: { DATABASE_URL: string; JWT_SECRET: string };
+}>();
+
+blogRouter.get("/bulk", (c) => {
+  return c.text("Hello Hono!");
+});
+
+blogRouter.get("/:id", (c) => {
+  return c.text("Hello Hono!");
+});
+
+blogRouter.post("/", (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env?.DATABASE_URL,
+  }).$extends(withAccelerate());
+
+  return c.text("Hello Hono!");
+});
+
+blogRouter.put("/", (c) => {
+  return c.text("Hello Hono!");
+});
