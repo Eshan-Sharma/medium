41e6a83c0fb9f662fef973e60d03157b8e478c89
eshan-sharma <43044334+Eshan-Sharma@users.noreply.github.com>
2024-04-13 18:08:36 +0530

    added blogs page to webiste

---
+++ frontend/src/pages/Blogs.tsx
@@ -0,0 +1,19 @@
+import { BlogCard } from "../component/BlogCard";
+
+export const Blogs = () => {
+  return (
+    <>
+      <div className="flex justify-center">
+        <div className="flex justify-center max-w-xl">
+          <BlogCard
+            id={1}
+            authorName="Eshan"
+            publishedDate="28 June 2024"
+            title="How an ugly single page website makes $5000 a month without affiliate marketting"
+            content="How an ugly single page website makes $5000 a month without affiliate marketting How an ugly single page website makes $5000 a month without affiliate marketting How an ugly single page website makes $5000 a month without affiliate marketting"
+          />
+        </div>
+      </div>
+    </>
+  );
+};
