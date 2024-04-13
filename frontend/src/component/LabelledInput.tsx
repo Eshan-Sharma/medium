6188b0c1a1dcb6e34c3b973623c8066c7673b8c8
eshan-sharma <43044334+Eshan-Sharma@users.noreply.github.com>
2024-04-13 12:58:43 +0530

    debug labelled input

--- frontend/src/component/LabelledInput.tsx
+++ frontend/src/component/LabelledInput.tsx
@@ -4,18 +4,19 @@ export const LabelledInput = ({
   label,
   placeholder,
   onChange,
+  type,
 }: LabelledInputType) => {
   return (
     <>
       <div>
-        <label className="block mb-2 text-sm font-medium text-black ">
+        <label className="block mb-2 text-sm font-semibold text-black pt-4">
           {label}
         </label>
         <input
           onChange={onChange}
-          type="text"
+          type={type}
           id="first_name"
-          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
           placeholder={placeholder}
           required
         />
@@ -28,4 +29,5 @@ interface LabelledInputType {
   label: string;
   placeholder: string;
   onChange: (e: ChangeEvent<HTMLInputElement>) => void;
+  type: string;
 }
