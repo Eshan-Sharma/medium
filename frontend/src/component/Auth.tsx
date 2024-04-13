e5ef6724808b8a10570d538ca73c3e8fb81ef1fc
eshan-sharma <43044334+Eshan-Sharma@users.noreply.github.com>
2024-04-13 12:59:08 +0530

    added and debugged auth component

--- frontend/src/component/Auth.tsx
+++ frontend/src/component/Auth.tsx
@@ -11,37 +11,66 @@ export const Auth = ({ type }: { type: "signup" | "signin" }) => {
   });
   return (
     <>
-      <div className=" h-screen flex justify-center flex-col">
+      <div className="h-screen flex justify-center flex-col">
         <div className="flex justify-center">
           <div>
-            <div className="text-3xl font-extrabold">Create an account</div>
-            <div className="text-slate-400 ">
-              Already have an account?
-              <Link to={"/signin"} className="pl-2 underline">
-                Login
-              </Link>
+            <div className="px-10">
+              <div className="text-3xl font-extrabold">Create an account</div>
+              <div className="text-slate-500">
+                {type === "signin"
+                  ? "Don't have an account?"
+                  : "Already have an account?"}
+                <Link
+                  className="pl-2 underline"
+                  to={type === "signin" ? "/signup" : "/signin"}
+                >
+                  {type === "signin" ? "Sign up" : "Sign in"}
+                </Link>
+              </div>
             </div>
-            <LabelledInput
-              label="Username"
-              placeholder="Eshan Sharma..."
-              onChange={(e) => {
-                setPostInputs({ ...postInputs, name: e.target.value });
-              }}
-            />
-            <LabelledInput
-              label="Email"
-              placeholder="m@example.com"
-              onChange={(e) => {
-                setPostInputs({ ...postInputs, email: e.target.value });
-              }}
-            />
-            <LabelledInput
-              label="Password"
-              placeholder=""
-              onChange={(e) => {
-                setPostInputs({ ...postInputs, password: e.target.value });
-              }}
-            />
+            <div className="pt-8">
+              {type === "signup" ? (
+                <LabelledInput
+                  label="Name"
+                  type="text"
+                  placeholder="Eshan Sharma..."
+                  onChange={(e) => {
+                    setPostInputs({
+                      ...postInputs,
+                      name: e.target.value,
+                    });
+                  }}
+                />
+              ) : null}
+              <LabelledInput
+                label="Username"
+                placeholder="example@gmail.com"
+                type="text"
+                onChange={(e) => {
+                  setPostInputs({
+                    ...postInputs,
+                    email: e.target.value,
+                  });
+                }}
+              />
+              <LabelledInput
+                label="Password"
+                type="password"
+                placeholder="123456"
+                onChange={(e) => {
+                  setPostInputs({
+                    ...postInputs,
+                    password: e.target.value,
+                  });
+                }}
+              />
+              <button
+                type="button"
+                className="mt-8 w-full text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
+              >
+                {type === "signup" ? "Sign up" : "Sign in"}
+              </button>
+            </div>
           </div>
         </div>
       </div>
