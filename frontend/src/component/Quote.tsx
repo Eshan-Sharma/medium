4ee8761020cb73428cc10b7778fbdfc9cd55b724
eshan-sharma <43044334+Eshan-Sharma@users.noreply.github.com>
2024-04-13 09:19:28 +0530

    added quote component

---
+++ frontend/src/component/Quote.tsx
@@ -0,0 +1,22 @@
+export const Quote = () => {
+  return (
+    <>
+      <div className="bg-slate-200 h-screen flex justify-center flex-col">
+        <div className="flex justify-center">
+          <div className="max-w-md ">
+            <div className="mb-5 text-3xl font-bold">
+              "The customer service I received was exceptional. The support team
+              went above and beyond to address my concerns"
+            </div>
+            <div className="max-w-md text-xl font-semibold text-left">
+              Julies Winfield
+            </div>
+            <div className="max-w-md text-sm font-ligfht text-slate-400">
+              CEO | Acme corp
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
