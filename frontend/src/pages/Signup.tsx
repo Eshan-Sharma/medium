ff2a8a946a4f5740abc03b431dbfd8b3cda07916
eshan-sharma <43044334+Eshan-Sharma@users.noreply.github.com>
2024-04-13 12:58:55 +0530

    added signup signin pages

--- frontend/src/pages/Signup.tsx
+++ frontend/src/pages/Signup.tsx
@@ -3,11 +3,11 @@ import { Auth } from "../component/Auth";
 export const Signup = () => {
   return (
     <>
-      <div className="grid grid-cols-2">
+      <div className="grid grid-cols-1 lg:grid-cols-2">
         <div>
           <Auth type="signup" />
         </div>
-        <div className="invisible lg:visible">
+        <div className="hidden lg:block">
           <Quote />
         </div>
       </div>
