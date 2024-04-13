a891332076bb2ca32975daeab3ec92f041ab406b
eshan-sharma <43044334+Eshan-Sharma@users.noreply.github.com>
2024-04-13 12:37:13 +0530

    added labelled input component

---
+++ frontend/src/component/LabelledInput.tsx
@@ -0,0 +1,31 @@
+import { ChangeEvent } from "react";
+
+export const LabelledInput = ({
+  label,
+  placeholder,
+  onChange,
+}: LabelledInputType) => {
+  return (
+    <>
+      <div>
+        <label className="block mb-2 text-sm font-medium text-black ">
+          {label}
+        </label>
+        <input
+          onChange={onChange}
+          type="text"
+          id="first_name"
+          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          placeholder={placeholder}
+          required
+        />
+      </div>
+    </>
+  );
+};
+
+interface LabelledInputType {
+  label: string;
+  placeholder: string;
+  onChange: (e: ChangeEvent<HTMLInputElement>) => void;
+}
