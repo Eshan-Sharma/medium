8e451dd5b5a143166d037c43293c2af1c9cf4cc1
eshan-sharma <43044334+Eshan-Sharma@users.noreply.github.com>
2024-04-13 12:38:22 +0530

    added auth to signup

--- frontend/src/pages/Signup.tsx
+++ frontend/src/pages/Signup.tsx
@@ -1,10 +1,12 @@
 import { Quote } from "../component/Quote";
-
+import { Auth } from "../component/Auth";
 export const Signup = () => {
   return (
     <>
       <div className="grid grid-cols-2">
-        <div></div>
+        <div>
+          <Auth type="signup" />
+        </div>
         <div className="invisible lg:visible">
           <Quote />
         </div>
