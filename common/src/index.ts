2b1c7b0dfd6c3c44c61ebff0f6cd63c48c7edae3
eshan-sharma <43044334+Eshan-Sharma@users.noreply.github.com>
2024-04-13 07:03:29 +0530

    added common zod validation

---
+++ common/src/index.ts
@@ -0,0 +1,14 @@
+import z from "zod";
+
+export const signupInput = z.object({
+  email: z.string().email(),
+  name: z.string().optional(),
+  password: z.string().min(6),
+});
+export type SignupInput = z.infer<typeof signupInput>;
+
+export const postInput = z.object({
+  title: z.string(),
+  content: z.string(),
+});
+export type PostInput = z.infer<typeof postInput>;
