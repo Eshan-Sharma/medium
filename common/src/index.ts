828f0534d08ce6bdccb329da489cac2352733fa2
eshan-sharma <43044334+Eshan-Sharma@users.noreply.github.com>
2024-04-13 07:12:02 +0530

    added signin zod object

--- common/src/index.ts
+++ common/src/index.ts
@@ -7,6 +7,13 @@ export const signupInput = z.object({
 });
 export type SignupInput = z.infer<typeof signupInput>;
 
+export const signinInput = z.object({
+  email: z.string().email(),
+  name: z.string().optional(),
+  password: z.string().min(6),
+});
+export type SigninInput = z.infer<typeof signinInput>;
+
 export const postInput = z.object({
   title: z.string(),
   content: z.string(),
